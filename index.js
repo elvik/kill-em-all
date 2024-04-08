02c7272c6910c2884ae5000346e98d0c3b130dc4
unknown <elvik>
2024-04-08 23:08:58 +0200

    Format code

--- index.js
+++ index.js
@@ -55,49 +55,48 @@ const MAX_COLS = 20;
 const SHIP_WIDTH = 2;
 
 const state = {
-    shipPositionX: 3,
+  shipPositionX: 3,
 };
 
-const gameArea = document.getElementById('game_area');
+const gameArea = document.getElementById("game_area");
 
 function clear() {
-    while (gameArea.firstChild) {
-        gameArea.firstChild.remove();
-    }
+  while (gameArea.firstChild) {
+    gameArea.firstChild.remove();
+  }
 }
 
 function drawShip(shipPositionX) {
-    const lastRow = gameArea.childNodes[MAX_ROWS - 1];
-    for(let i = 0; i < SHIP_WIDTH; i++) {
-        lastRow.childNodes[i + shipPositionX].classList.add('ship');
-    }
+  const lastRow = gameArea.childNodes[MAX_ROWS - 1];
+  for (let i = 0; i < SHIP_WIDTH; i++) {
+    lastRow.childNodes[i + shipPositionX].classList.add("ship");
+  }
 }
 
 function draw() {
-    for (let i = 0; i < MAX_ROWS; i++) {
-        let row = document.createElement("div");
-        row.className = "row";
-        gameArea.appendChild(row);
-        for (let j = 0; j < MAX_COLS; j++) {
-            let col = document.createElement("div");
-            col.className = "col";
-            row.appendChild(col);
-            // if (array[i][j] !== 0) {
-            //     col.style.backgroundColor = colors[array[i][j] - 1];
-            // }
-        }
+  for (let i = 0; i < MAX_ROWS; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+    gameArea.appendChild(row);
+    for (let j = 0; j < MAX_COLS; j++) {
+      let col = document.createElement("div");
+      col.className = "col";
+      row.appendChild(col);
+      // if (array[i][j] !== 0) {
+      //     col.style.backgroundColor = colors[array[i][j] - 1];
+      // }
     }
-    drawShip(state.shipPositionX);
+  }
+  drawShip(state.shipPositionX);
 }
-
 
 draw();
 
 document.addEventListener("keydown", function (event) {
-    const key = event.key;
-    console.log("key", key);
+  const key = event.key;
+  console.log("key", key);
 
-if (key === "ArrowRight") {
+  if (key === "ArrowRight") {
     if (gameArea - getCurrentFigure()[0].length) {
       if (canPutFigure(areaState, getCurrentFigure(), x, y + 1)) {
         y = y + 1;
@@ -113,3 +112,7 @@ if (key === "ArrowRight") {
         y = y - 1;
 
         redraw();
+      }
+    }
+  }
+});
